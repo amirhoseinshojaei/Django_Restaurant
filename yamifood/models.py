5c4a391ef4dd5efa76b6c61cdf50834e71fdd7b7
amirhosein <amirhoseinbu80@gmail.com>
2023-10-28 00:35:24 +0330

    added status field to models

--- yamifood/models.py
+++ yamifood/models.py
@@ -13,3 +13,7 @@ class Food(models.Model):
     image = models.ImageField(upload_to= 'foods/')
     type = models.CharField(max_length=50,choices= FOOD_TYPE, default="Lunch")
     pub_date = models.DateTimeField(auto_now= False , auto_now_add=True)
+    status = models.BooleanField(default= False)
+
+    def __str__(self):
+        return self.name
