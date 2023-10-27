698704a1ac6801f27e17819b5f825a40512ae3c2
amirhosein <amirhoseinbu80@gmail.com>
2023-10-28 00:42:12 +0330

    create views for foods

--- yamifood/views.py
+++ yamifood/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Food
+from django.views.generic import ListView
 # Create your views here.
+class FoodList(ListView):
+    queryset = Food.objects.filter(status=True)
+    context_object_name = 'objects-list'
+    template_name = 'food/food_list/html'
