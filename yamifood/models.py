3b374aff342395947c23649c670701bb84ce1956
amirhosein <amirhoseinbu80@gmail.com>
2023-10-28 00:11:33 +0330

    create models for foods

--- yamifood/models.py
+++ yamifood/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    FOOD_TYPE = [
+        ("Lunch",),
+        ("Drinks",),
+        ("Dinner",),
+    ]
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length= 100)
+    price = models.PositiveIntegerField()
+    image = models.ImageField(upload_to= 'foods/')
+    type = models.CharField(max_length=50,choices= FOOD_TYPE, default="Lunch")
+    pub_date = models.DateTimeField(auto_now= False , auto_now_add=True)
