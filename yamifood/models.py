d9e722b7c43031943d4e5af04fe60bb82c6d1118
amirhosein <amirhoseinbu80@gmail.com>
2023-10-29 00:00:40 +0330

    create models for staf

--- yamifood/models.py
+++ yamifood/models.py
@@ -25,3 +25,13 @@ class Gallery(models.Model):
 
     def __str__(self):
         return "{},{}".format(self.image_name,self.date)
+    
+class Staf(models.Model):
+    name = models.CharField(max_length=55)
+    work_fields = models.CharField(max_length=50)
+    description = models.TextField(max_length=100)
+    image = models.ImageField(upload_to= 'staf/')
+    date = models.DateTimeField(auto_now=False,auto_now_add=True)
+
+    def __str__(self):
+        return (self.name,self.work_fields)
