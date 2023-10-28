e4ba8147da60c31f25b2a4df0a184e1f3595e2c5
amirhosein <amirhoseinbu80@gmail.com>
2023-10-28 23:54:15 +0330

    register models

--- yamifood/admin.py
+++ yamifood/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Food
+from .models import Food , Gallery
 # Register your models here.
 class FoodAdmin(admin.ModelAdmin):
     list_display=[
@@ -13,3 +13,14 @@ class FoodAdmin(admin.ModelAdmin):
     ordering = ["pub_date"]
 
 admin.site.register(Food,FoodAdmin)
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display =[
+        'name',
+        'date'
+    ]
+    list_filter = ['date']
+    search_fields = ['name']
+    ordering = ['date']
+
+admin.site.register(GalleryAdmin,Gallery)
