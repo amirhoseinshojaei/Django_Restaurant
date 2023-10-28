394ec00a382963e7c9a234ab54639392596d76a2
amirhosein <amirhoseinbu80@gmail.com>
2023-10-29 00:24:04 +0330

    create views for staff

--- yamifood/views.py
+++ yamifood/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
-from .models import Food , Gallery
+from .models import Food , Gallery , Staff
 from django.views.generic import ListView
 # Create your views here.
 class FoodList(ListView):
     queryset = Food.objects.filter(status=True)
     context_object_name = 'objects-list'
-    template_name = 'food/food_list/html'
+    template_name = 'food/index.html'
 
 def gallery_list(request):
     obj = Gallery.objects.all()
     context = {
         'obj':obj
     }
-    return render('food/gallery.html',context)
+    return render('food/index.html',context)
+
+class StafList(ListView):
+    model = Staff
+    context_object_name = 'objects_staff'
+    template_name = 'staff/index.html'
