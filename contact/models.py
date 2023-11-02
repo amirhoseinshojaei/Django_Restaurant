d3bc4621fc7a1916ad617a4353534ed791bc43aa
amirhosein <amirhoseinbu80@gmail.com>
2023-11-02 16:50:48 +0330

    create models for contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    person_number =[
+        ("a",'1'),
+        ("b",'2'),
+        ("c",'3'),
+        ("d",'4'),
+        ("e",'5'),
+        ("f",'6'),
+        ("g",'7')
+    ]
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+    message = models.TextField(max_length=255)
+    person = models.CharField(max_length=50,choices=person_number,default='1')
