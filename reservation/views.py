803090360fc8b22d2ec91824ade6594a30e7acf2
amirhosein <amirhoseinbu80@gmail.com>
2023-11-02 16:11:34 +0330

    create views for resserve form

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Reservation
+from .forms import ReservationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 # Create your views here.
+def reservation_form(request):
+    reservation_form = ReservationForm
+    if request.method=='POST':
+        reservation_form = ReservationForm(request.POST)
+        if reservation_form.is_valid:
+            reservation_form.save()
+    else:
+        reservation_form = ReservationForm()
+    context = {
+        "form":reservation_form
+    }
+
+    return render(request,'reserve/reservation.html',context)
