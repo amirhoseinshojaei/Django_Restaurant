21401fbaa2f7f3291bda5a630b6e4d0a5a4ffb79
amirhosein <amirhoseinbu80@gmail.com>
2023-11-02 12:48:06 +0330

    create reservation models

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Reservation(models.Model):
+    person_number =[
+        ("a",'1'),
+        ("b",'2'),
+        ("c",'3'),
+        ("d",'4'),
+        ("e",'5'),
+        ("f",'6'),
+        ("g",'7')
+    ]
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    number = models.PositiveIntegerField()
+    date = models.DateField(auto_now=False,auto_now_add=True)
+    time = models.TimeField(auto_now=False,auto_now_add=True)
+    person = models.CharField(max_length=255,choices=person_number,default='1')
+
+    def __str__(self):
+        return "{},{}".format(self.name,self.date)
