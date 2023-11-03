682a25bcedbf821af37f990c8b5faf2deb594f55
amirhosein <amirhoseinbu80@gmail.com>
2023-11-04 00:31:00 +0330

    create views for contact

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from .models import Contact
+from .forms import ContacForm
 # Create your views here.
+def contact_form(request):
+    contact_form = ContacForm
+    if request.method == 'POST':
+        contact_form = ContacForm(request.POST)
+        if contact_form.is_valid:
+            contact_form.save()
+    else:
+        contact_form = ContacForm()
+    
+    context={
+        "contact": contact_form
+    }
+
+    return render (request,'contact/form.html',context)
