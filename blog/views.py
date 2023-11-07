8275b11f5ca5a40792704d113633107fca311fa5
amirhosein <amirhoseinbu80@gmail.com>
2023-11-07 23:08:05 +0330

    create blog_list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from .models import (Blog,Category,Tag,Comment)
+from django.core.paginator import Paginator
 # Create your views here.
+def blog_list(request):
+    blogs = Blog.objects.all()
+    context = {
+        'blogs':blogs
+    }
+    paginator = Paginator(blogs,6)
+    page_number = request.Get.get('page')
+    blog_list = paginator.get_page('page_number')
+
+    return render (request,'blogs/blog_list.html',context)
+
+
+
