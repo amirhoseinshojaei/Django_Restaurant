cb727abd9397f1f2660d54f4f9bc6d6a43e9d7b8
amirhosein <amirhoseinbu80@gmail.com>
2023-11-09 15:34:06 +0330

    create blog_detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import (Blog,Category,Tag,Comment)
 from django.core.paginator import Paginator
+from .forms import CommentForm
+from django.http import HttpResponseRedirect
 # Create your views here.
 def blog_list(request):
     blogs = Blog.objects.all()
@@ -12,6 +14,34 @@ def blog_list(request):
     blog_list = paginator.get_page('page_number')
 
     return render (request,'blogs/blog_list.html',context)
+
+def blog_detail(request,slug):
+    blog = get_object_or_404(blog,slug=slug)
+    category = Category.objects.all()
+    tags = Tag.objects.all().filter(tags = blog)
+    recents = Blog.objects.all().order_by("date")[:4]
+    comments = Comment.objects.all()
+    context={
+        "blog":blog,
+        "category":category,
+        "tags":tags,
+        "recents":recents,
+        "comments":comments
+    }
+
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.blog= blog
+            obj.save()
+            return HttpResponseRedirect(request.path_info)
+        
+        else:
+            form = CommentForm()
+
+    return render(request,'blogs/blog_detail.html',context)
+
 
 
 
